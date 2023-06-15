f8a6c482894471600041a7a1453f0ec1c2753c6a
thejayadad <74875595+thejayadad@users.noreply.github.com>
2023-06-15 08:46:26 -0400

    update

--- nextnotes/src/app/page.js
+++ nextnotes/src/app/page.js
@@ -1,59 +1,54 @@
 'use client'
-import React, {useState, useEffect} from 'react'
+import React, {useState} from 'react'
 import Axios from 'axios'
 
 
-export async function fetchBlogs(){
-  const res = await Axios.get('http://localhost:3000/api/todos', {cache: 'no-store'})
-
-  return res.data;
-}
-
 
 export default async function Home() {
-  const [todos, setTodos] = useState([]);
   const [title, setTitle] = useState('');
-  const [todoId, setTodoId] = useState('');
-  const [visibility, setVisibility] = useState(false);
+
   const [todo, setTodo] = useState('');
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await fetchBlogs();
-      setTodos(data);
-    };
-    fetchData();
-  }, [fetchBlogs]);
+  const todos = await fetchBlogs()
 
-  const editForm = (title, todo, todoId) => {
-      setVisibility(visibility => !visibility)
-      setTitle(title)
-      setTodo(todo)
-      setTodoId(todoId)
-  }
+ 
 
-  const updateTodo = async (todoId) =>{
-      const todoObj = {
-          title: title,
-          todo: todo
-        }
-        console.log(todoObj)
-       await Axios.put(`/api/updateTodo?id=${todoId}`, todoObj)
-        .then(()=>{
-          window.location.reload(false)
-        })
-  }
+
+  // const editForm = (title, todo, todoId) => {
+  //     setVisibility(visibility => !visibility)
+  //     setTitle(title)
+  //     setTodo(todo)
+  //     setTodoId(todoId)
+  // }
+
+  // const updateTodo = async (todoId) =>{
+  //     const todoObj = {
+  //         title: title,
+  //         todo: todo
+  //       }
+  //       console.log(todoObj)
+  //      await Axios.put(`/api/updateTodo?id=${todoId}`, todoObj)
+  //       .then(()=>{
+  //         window.location.reload(false)
+  //       })
+  // }
 
   
-  const handleSubmit = () =>{
+  const handleSubmit = async (event) => {
+    event.preventDefault();
     const todoObj = {
       title: title,
       todo: todo
     }
-    Axios.post('/api/newTodo', todoObj)
-    .then(()=>{
-      alert('Todo added')
-    })
+    try {
+      await Axios.post('/api/newTodo', todoObj)
+      .then(()=>{
+        alert('Todo added')
+      })
+   
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   return (
@@ -65,12 +60,12 @@ export default async function Home() {
         <input
           type="text"
           required
-          onChange={(event)=>setTitle(event.target.value)}
+          onChange={(event) => setTitle(event.target.value)}
           placeholder='Title'
         />
         <input
         type='text'
-        onChange={(event)=>setTodo(event.target.value)}
+        onChange={(event) => setTodo(event.target.value)}
         placeholder='TodDo'
         required
         />
@@ -79,18 +74,24 @@ export default async function Home() {
       </section>
       <section>
         <h2>All Todo's</h2>
-        {todos &&
+        {
           todos.map((element) => {
             return (
               <div key={element._id}>
                 <h2>{element.title}</h2>
                 <p>{element.todo}</p>
-                <button onClick={() => editForm(element.title, element.todo, element._id)}>Edit</button>
+                {/* <button
+                  onClick={() =>
+                    editForm(element.title, element.todo, element._id)
+                  }
+                >
+                  Edit
+                </button> */}
               </div>
             );
           })}
       </section>
-      
+{/*       
       {visibility && <div >
         <h1>Update Todo</h1>
         <form>
@@ -105,7 +106,7 @@ export default async function Home() {
           <button type="submit"  onClick={()=>updateTodo(todoId)}>Submit</button>
           <button onClick={()=>setVisibility(visibility => !visibility)}>Cancel</button>
         </form>
-      </div>}
+      </div>} */}
     </main>
   )
 }
@@ -114,3 +115,13 @@ export default async function Home() {
 <h2>{element.title}</h2>
 <p>{element.todo}</p>
 </div> */}
+
+
+export async function fetchBlogs(){
+  const res = await fetch('http://localhost:3000/api/todos', {cache: 'no-store'})
+
+  return res.json()
+}
+
+
+
